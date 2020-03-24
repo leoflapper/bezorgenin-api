fbcc2ff842bd8d358f068d805f2cca43a4f66df8
Leo Flapper <dominicus1991@gmail.com>
2020-03-24 20:26:10 +0100

    Fixed mobile styling of home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,34 +3,35 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <h1>Welkom bij {{ config('app.name') }}</h1>
+        <div class="col-xs-12">
+            <h1>Welkom bij {{ config('app.name') }}</h1>
 
-        @if($dashboardData)
-            <div class="row" style="margin-top: 30px; margin-bottom: 30px;">
-                @foreach($dashboardData as $data)
-                    <div class="col-xs-12 col-md-4">
-                        <div style="padding: 20px; border: 1px solid; text-align: center">
-                            <a href="{{$data['link']}}" style="color: black;">
-                                <h4><strong>{{ $data['title'] }}</strong></h4>
-                                <h3>{{$data['amount']}}</h3>
-                            </a>
+            @if($dashboardData)
+                <div class="row" style="margin-top: 30px; margin-bottom: 30px;">
+                    @foreach($dashboardData as $data)
+                        <div class="col-xs-6 col-md-4">
+                            <div style="padding: 20px; border: 1px solid; text-align: center;">
+                                <a href="{{$data['link']}}" style="color: black;">
+                                    <h4><strong>{{ $data['title'] }}</strong></h4>
+                                    <h3>{{$data['amount']}}</h3>
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
-            </div>
-        @endif
+                    @endforeach
+                </div>
+            @endif
 
-        <p>
-            Dit is je beheerpaneel waar je jouw bedrijfsinformatie en producten kunt beheren. Ook vind je hier het volledige overzicht van bestellingen. <br>
-            Volg onderstaande stappen om je bedrijfspagina in te richten. Via <i class="glyphicon glyphicon-edit"></i> kun je producten wijzigen, via <i class="fa fa-plus"></i> toevoegen en via <i class="glyphicon glyphicon-trash"></i> kun je het verwijderen.
-        </p>
+            <p>
+                Dit is je beheerpaneel waar je jouw bedrijfsinformatie en producten kunt beheren. Ook vind je hier het volledige overzicht van bestellingen. <br>
+                Volg onderstaande stappen om je bedrijfspagina in te richten. Via <i class="glyphicon glyphicon-edit"></i> kun je producten wijzigen, via <i class="fa fa-plus"></i> toevoegen en via <i class="glyphicon glyphicon-trash"></i> kun je het verwijderen.
+            </p>
             <br>
 
             @if($actions && count($actions) >= 1)
 
-                    @foreach($actions as $action)
+                @foreach($actions as $action)
                     <div class="row">
-                        <div class="col-xs-6">
+                        <div class="col-xs-12 col-lg-12">
                             <div class="alert @if(!$action['done']) alert-{{ isset($action['type']) ? $action['type'] : 'warning' }} @else alert-success @endif">
                                 <h4>{{ $loop->index + 1 }}. {{$action['title']}}</h4>
                                 <p>{!! $action['description'] !!}</p>
@@ -38,7 +39,7 @@
                             </div>
                         </div>
                     </div>
-                    @endforeach
+                @endforeach
 
             @endif
 
@@ -50,6 +51,8 @@
             <p>Je bestellingen komen per e-mail binnen op het e-mailadres dat je bij <a href="{{route('companies.index')}}">Bedrijven</a> hebt ingevuld.</p>
 
             <h3>Betalingen</h3><p>De betalingen kun je zelf regelen via Tikkie of een betaalverzoek. Op de bestelbevestiging in je e-mailbox komt een link om direct een WhatsAppbericht naar de klant te sturen.</p>
+
+        </div>
     </div>
 </div>
 @endsection
